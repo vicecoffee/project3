a7025ef8b9d27f0bfc9656f47b1dd6c7252c9f4b
Valerie Yi Coffee <vcoffee@hsph.harvard.edu>
2018-07-31 01:46:14 +0000

    Worked on menu page buttons, navbar, and added django's default auth urls.

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("menu/", views.menu, name="menu")
+    path("menu/", views.menu, name="menu"),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
